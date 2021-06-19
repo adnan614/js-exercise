9d6793f4d12e1a56e6d7e1676658f592487ff44e
Adnan Faruque Alvi <adnanfaruque23@gmail.com>
2021-06-19 21:34:41 +0600

    rr

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// var arr = [4, 5, 6, 1, 2, 3]
-// sum = 0
+var arr = [4, 5, 6, 1, 2, 3]
+sum = 0
 
 
-// for (var i = 0; i < arr.length; i++)
-// {
+for (var i = 0; i < arr.length; i++)
+{
    
-//     sum = sum + arr[i];
+    sum = sum + arr[i];
 
     
-// }
+}
 
 
 
